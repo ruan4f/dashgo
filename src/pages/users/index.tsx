aa06e5686622b580cccdc88167bb3c19e8175320
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-22 12:35:15 -0300

    Create components to Header

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
+
 export default function UserList() {
   return (
     <Box>
