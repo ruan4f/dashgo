0b8667036600be5f1e9980fe2e77d6004107c09f
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-24 22:33:30 -0300

    Add yup resolver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
 import { Input } from '../components/Form/Input';
 
 type SignInFormData = {
@@ -7,21 +10,39 @@ type SignInFormData = {
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema) 
+  });
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(values);
   }
 
   return (
     <Flex w='100vw' h='100vh' align='center' justify='center'>
       <Flex as='form' width='100%' maxWidth={360} bg='gray.800' p='8' borderRadius={8} flexDirection='column' onSubmit={handleSubmit(handleSignIn)}>
         <Stack spacing={4}>
-          <Input name='email' type='email' label='E-mail' {...register("email")} />
-          <Input name='password' type='password' label='Senha' {...register("password")} />
+          <Input
+            name='email'
+            type='email'
+            label='E-mail'
+            error={errors.email}
+            {...register("email", { required: 'E-mail obrigatório' })}
+          />
+          <Input
+            name='password'
+            type='password'
+            label='Senha'
+            error={errors.password}
+            {...register("password")}
+          />
         </Stack>
 
         <Button type='submit' mt='6' colorScheme='pink' size='lg' isLoading={formState.isSubmitting}>
